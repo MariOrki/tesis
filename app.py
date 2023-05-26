c1156d4bacdf9b811c19834d4da747d2303876b0
MariOrki <mari.bruningg@gmail.com>
2023-05-26 13:06:16 -0400

    añadimos datos

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import streamlit as st
+import pandas as pd
+
+def main():
+    st.title('Titulo :thinking_face:')
+    st.write('Mensajito.')
+
+    boton = st.button('Mensaje de apretar')
+
+    st.write(boton)
+
+    st.balloons()
 
 
-st.title('Titulo :thinking_face:')
-st.write('Mensajito.')
 
-boton = st.button('Mensaje de apretar')
+def load_data():
+    df = pd.read_csv('NLU_nation.csv')
+    return df 
 
-st.write(boton)
 
-st.balloons()
+main()
