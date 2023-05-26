cfd9f7327951e7fb6bb65e296bd7fece719bfbb2
MariOrki <mari.bruningg@gmail.com>
2023-05-26 13:19:30 -0400

    actualizar app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 def main():
     st.title('Titulo :thinking_face:')
@@ -15,9 +16,22 @@ def main():
     st.markdown('---')
     st.table(df)
 
+    plot_serie_tiempo(df, "Uso de agua no relacionado con la tierra en Chile")
+    plot_serie_tiempo(df, "Non Land Water Use in Chile")
+
+  
 
 
+def plot_serie_tiempo(df: pd.DataFrame, title: str) -> None:
+    fig1 = px.line(df, x='Years', y='NLU', title=title, labels={'Years':'Años', 'NLU' : 'Uso de agua [m³/s]'})
+    # cambiai eje x
+    # titulo
+    #blabla
+    st.plotly_chart(fig1)
 
+
+#@decoradores
+@st.cache_data #le dice a streamlit que solo lo lea la primera vez y despu{es lo guarde en memoria}
 def load_data():
     df = pd.read_csv('NLU_nation.csv')
     return df 
